ed3deee767a6edd95d48741f87c353073dabe46e
ghrguse <forregisteruse@gmail.com>
2016-08-28 16:41:47 +0800

    Revert "ulua"
    This reverts commit f28f68f0a5654097fd3ff3f13858a26c39c907da.

--- Assets/uLua/Examples/09_Delegate/09_Delegate_1/TestLuaDelegate.cs
+++
@@ -1,7 +0,0 @@
-﻿using UnityEngine;
-using System.Collections;
-
-public class TestLuaDelegate : MonoBehaviour {
-    public delegate void VoidDelegate(GameObject go);
-    public VoidDelegate onClick;
-}
